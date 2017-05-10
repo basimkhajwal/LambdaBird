c21fd36d1301d42944ae1cc70f43426c7871c112
Basim Khajwal <basimkhajwal@gmail.com>
2017-05-10 23:21:54 +0100

    Started the actual development, Elm seems to be quite nice

--- Main.elm
+++ Main.elm
@@ -1,39 +1,99 @@
-import Html exposing (Html, button, div, text, program)
-import Html.Events exposing (onClick)
+import Html exposing (Html, program)
+import Task
+
+import Window
+import AnimationFrame
+
+import Color exposing (..)
+import Collage exposing (..)
+import Element exposing (..)
+import Text exposing (..)
+
+-- CONSTANTS
+
+canvasSize : Window.Size
+canvasSize =
+    { width = 1000
+    , height = 600
+    }
+
+menuBackground : Form
+menuBackground =
+    rect (toFloat canvasSize.width) (toFloat canvasSize.height)
+        |> filled (rgb 230 230 230)
 
 -- MODEL
-type alias Model = Bool
+type GameState
+    = Menu
+    | Play
+
+type alias Model =
+    { size : Window.Size
+    , state : GameState
+    }
 
 init : ( Model, Cmd Msg )
-init = ( False, Cmd.none )
+init =
+    let
+        resizeTask : Result x Window.Size -> Msg
+        resizeTask res =
+            case res of
+                Ok size -> Resize size
+                Err _ -> NoOp
+
+        startModel : Model
+        startModel =
+            { size = Window.Size 0 0
+            , state = Menu
+            }
+    in
+        ( startModel
+        , Task.attempt resizeTask Window.size
+        )
 
 -- MESSAGES
-type Msg = Expand | Collapse
+
+type Msg
+    = NoOp
+    | Resize Window.Size
+    | StartGame
 
 -- VIEW
 view : Model -> Html Msg
-view model =
-    if model then
-        div []
-            [ button [ onClick Collapse ] [ text "Collapse" ]
-            , text "Widget"
-            ]
-    else
-        div []
-            [ button [ onClick Expand ] [ text "Expand" ] ]
+view model = menuModel model
+
+menuModel : Model -> Html Msg
+menuModel model =
+    toHtml <|
+    container model.size.width model.size.height middle <|
+    collage canvasSize.width canvasSize.height
+        [ menuBackground
+        , move (0, 250) <|
+          text (
+            fromString "Lambda Bird"
+            |> monospace
+            |> Text.height 35
+            |> Text.color darkRed
+            |> bold
+          )
+        ]
 
 -- UPDATE
 
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        Expand -> ( True, Cmd.none )
-        Collapse -> ( False, Cmd.none )
+        NoOp -> (model, Cmd.none)
+        Resize size -> ({ model | size = size}, Cmd.none)
+        StartGame -> ({ model | state = Play}, Cmd.none)
 
 -- SUBSCRIPTIONS
 
 subscriptions : Model -> Sub Msg
-subscriptions model = Sub.none
+subscriptions model
+    = Sub.batch
+        [ Window.resizes Resize
+        ]
 
 -- MAIN
 main =
