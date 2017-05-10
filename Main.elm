77a33b59c02eed8c54449f2aa962b76ccb7ae0a0
Basim Khajwal <basimkhajwal@gmail.com>
2017-05-10 17:19:42 +0000

    Made a little bit of changes

--- Main.elm
+++ Main.elm
@@ -2,22 +2,15 @@ import Html exposing (Html, button, div, text, program)
 import Html.Events exposing (onClick)
 
 -- MODEL
-
-type alias Model =
-    Bool
+type alias Model = Bool
 
 init : ( Model, Cmd Msg )
-init =
-    ( False, Cmd.none )
+init = ( False, Cmd.none )
 
 -- MESSAGES
-
-type Msg
-    = Expand
-    | Collapse
+type Msg = Expand | Collapse
 
 -- VIEW
-
 view : Model -> Html Msg
 view model =
     if model then
@@ -34,17 +27,13 @@ view model =
 update : Msg -> Model -> ( Model, Cmd Msg )
 update msg model =
     case msg of
-        Expand ->
-            ( True, Cmd.none )
-
-        Collapse ->
-            ( False, Cmd.none )
+        Expand -> ( True, Cmd.none )
+        Collapse -> ( False, Cmd.none )
 
 -- SUBSCRIPTIONS
 
 subscriptions : Model -> Sub Msg
-subscriptions model =
-    Sub.none
+subscriptions model = Sub.none
 
 -- MAIN
 main =
